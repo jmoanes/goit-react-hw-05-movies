1737354db4f20d8887437822ded0d1de6d24c4ba
John jay Moanes <johnjaymoanes009@gmail.com>
2025-11-11 09:49:11 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
